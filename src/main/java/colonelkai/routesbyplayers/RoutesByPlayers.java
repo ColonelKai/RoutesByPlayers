ed8c8bcef683bca4ca557ec728806cc74b9bc5d9
ColonelKai <59742334+ColonelKai@users.noreply.github.com>
2022-04-30 16:22:40 +0300

    Initial commit

---
+++ src/main/java/colonelkai/routesbyplayers/RoutesByPlayers.java
@@ -0,0 +1,17 @@
+package colonelkai.routesbyplayers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoutesByPlayers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
